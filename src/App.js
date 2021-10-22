d5b8e30cec227edc42536ce6b2f4f589acf638a6
lassoued mohamed <lassouedmohamedd@gmail.com>
2021-10-22 11:39:20 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
+import Pictogramme from './Composants/Pictogramme'
+import Commentaire from './Composants/Commentaire'
+import 'font-awesome/css/font-awesome.min.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state={
+    msg : 'Bonne découverte des composants React "Components!!!"'
+  }
+  Changer = ()=>{
+    this.setState({
+        msg : "Premier dfi réussi"
+    })
+  }
+  render(){
+    return (
+
+      <div className="col-6 offset-3">
+        <div className="card">
+        <div className="card-block">
+        <div className="row">
+        <div className="col-2">
+        <img src="https://thomas-bart.com/wp-content/uploads/2017/03/developper-1680x1201.jpg" className="rounded float-left" className="img-thumbnail" width="80" height="70" />
+        </div>
+        <div className="col-10 profile-row">
+        <div className="row">
+        <a href="#"><h1> Débuter avec React </h1></a>
+        </div>
+        </div>
+        </div>
+        <p>{this.state.msg}</p>
+        
+        <div>
+        <Pictogramme mod={this.Changer}/>
+        </div>
+        </div>
+        <div className="card-footer text-muted">
+          <Commentaire />
+        </div>
+        </div>
+      </div>
+
+    );
+  }
 }
 
 export default App;
---
+++ src/Composants/Pictogramme.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Icone from './Icone'
+class Pictogramme extends React.Component {
+    
+    render() {
+        return (
+            
+            <div>
+              {/* Inclut le sous-composant LikeIcon à l'intérieur du composant Like*/}
+                <Icone />
+                <hr />
+                <div>
+                <button type="button" className="btn no-outline btn-secondary" onClick={this.props.mod} >
+                <i className="fa fa-thumbs-o-up fa-4 align-middle" aria-hidden="true"></i>
+                &nbsp;
+                <span className="align-middle">J'aime</span>
+                </button>
+            </div>
+  
+            </div>
+        )
+    }
+}
+export default Pictogramme
